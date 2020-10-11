851855b094c28d5de42fd9738468b002aa84a185
Pulkit0076 <71369943+Pulkit0076@users.noreply.github.com>
2020-10-11 13:04:01 +0530

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,71 @@
+$(document).ready( function(){
+$(".yellow").click( function(){
+$(".face").css("background", "#000066");
+$(".body").css("background", "#000066");
+$(".tail").css("background", "#000066");
+$(".ear").css("background", "#000066");
+$(".legs").css("background","#000066");
+$(".ear2").css("background","#000066")
+$(".shade").css("background","#f1f1f1")
+});
+    $(".blue").click( function(){
+   $(".face").css("background", "#ffaa00");
+$(".body").css("background", "#ffaa00");
+$(".tail").css("background", "#ffaa00");
+$(".ear").css("background", "#ffaa00");
+$(".legs").css("background","#ffaa00");
+$(".tongue").css("background","#ff6699");
+$(".shade").css("background","#000")
+$(".ear2").css("background","#ffe6b3")
+});
+
+$(".red").click( function(){
+$(".face").css("background", "#669999");
+$(".body").css("background", "#669999");
+$(".tail").css("background", "#669999");
+$(".ear").css("background", "#669999");
+$(".legs").css("background","#669999");
+$(".shade").css("background","#e0ebe0")  
+$(".ear2").css("background","#d1e0e0");
+});
+$(".teal").click( function(){
+$(".face").css("background", "#660033");
+$(".body").css("background", "#660033");
+$(".tail").css("background", "#660033");
+$(".ear").css("background", "#660033");
+$(".legs").css("background","#660033");
+$(".shade").css("background","#fff")  
+$(".ear2").css("background","#660033");
+});
+$(".pink").click( function(){
+$(".face").css("background", "#666633");
+$(".body").css("background", "#666633");
+$(".tail").css("background", "#666633");
+$(".ear").css("background", "#666633");
+$(".legs").css("background","#666633");
+$(".shade").css("background","#bbbb77")  
+$(".ear2").css("background","#bbbb77");
+});
+
+$(".purple").click( function(){
+$(".face").css("filter", "hue-rotate(-120deg)");
+$(".body").css("filter", "hue-rotate(-120deg)");
+$(".tail").css("filter", "hue-rotate(-120deg)");
+$(".ear2").css("background","pink");
+$(".ear").css("filter", "hue-rotate(-120deg)");
+});
+$(".one").click( function(){
+$(".face").css("filter", "hue-rotate(-180deg)");
+$(".body").css("filter", "hue-rotate(-180deg)");
+$(".tail").css("filter", "hue-rotate(-180deg)");
+$(".ear").css("filter", "hue-rotate(-180deg)");
+
+});
+$(".two").click( function(){
+$(".face").css("filter", "hue-rotate(-260deg)");
+$(".body").css("filter", "hue-rotate(-260deg)");
+$(".tail").css("filter", "hue-rotate(-260deg)");
+$(".ear").css("filter", "hue-rotate(-260deg)");
+$(".ear2").css("filter","hue-rotate(-260deg)");
+});
+});
